e5b58f41fd6341fb603774e0d4f8dc3f1cbc8bb2
Mamumimi <35428053+Mamumimi@users.noreply.github.com>
2025-09-03 22:31:10 -0400

    Switched detection method from game flags to finding the actual component, should be more compatible with multiplayer now

--- init.lua
+++ init.lua
@@ -1,8 +1,18 @@
 function OnPlayerSpawned(player_id)
-    if GameHasFlagRun("poly_safe_allies_fix") then return end
-    GameAddFlagRun("poly_safe_allies_fix")
+    local luaHomunculusComponents = EntityGetComponent(player_id, "LuaComponent", "homunculus_type")
+    if luaHomunculusComponents then
+        for _,component in ipairs(luaHomunculusComponents) do
+            if ComponentGetValue2(component,"script_polymorphing_to") == "mods/poly_safe_allies_fix/files/applier_entity_spawner.lua"
+                then
+                --GamePrintImportant("Already applied, skipping")
+                return
+            end
+        end
+    end
+    --GamePrintImportant("Not found, initializing now")
     EntityAddComponent2( player_id, "LuaComponent", {
         execute_every_n_frame=-1,
-        script_polymorphing_to="mods/poly_safe_allies_fix/files/applier_entity_spawner.lua"
+        script_polymorphing_to="mods/poly_safe_allies_fix/files/applier_entity_spawner.lua",
+        _tags="homunculus_type"
     })
 end
