7b0805f30f1f0e077c2bafd78b8a921a7de08c63
Mamumimi <35428053+Mamumimi@users.noreply.github.com>
2025-09-03 20:22:45 -0400

    Changed a string to an int

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ function OnPlayerSpawned(player_id)
     if GameHasFlagRun("poly_safe_allies_fix") then return end
     GameAddFlagRun("poly_safe_allies_fix")
     EntityAddComponent2( player_id, "LuaComponent", {
-        execute_every_n_frame="-1",
+        execute_every_n_frame=-1,
         script_polymorphing_to="mods/poly_safe_allies_fix/files/applier_entity_spawner.lua"
     })
 end
